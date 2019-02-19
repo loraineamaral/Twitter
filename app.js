3d4a8c50eed032168ac32e00abeca694cc3e480a
loraineamaral <loraineeamaral@gmail.com>
2019-02-19 13:28:18 -0300

    Versão 0.0.1

---
+++ app.js
@@ -0,0 +1,7 @@
+
+function printText(){
+    
+let userMessage = document.getElementById("message").value;
+document.getElementById("printedMessage").textContent = userMessage;  
+document.getElementById("textForm").reset(); 
+}                                               
