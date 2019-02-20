b3931e35d0fd874e322e6cdf29f71b3257e5796d
loraineamaral <loraineeamaral@gmail.com>
2019-02-20 13:05:36 -0300

    Botão e contador funcionando

--- app.js
+++ app.js
@@ -1,29 +1,35 @@
 
-function limite_textarea(valor) {
-    quant = 140;
+function counter(valor) {
+    limit = 140;
     total = valor.length;
-    if(total <= quant) {
-        resto = quant - total;
-        document.getElementById("cont").innerHTML = resto;
+    if(total <= limit) {
+        rest = limit - total;
+        document.getElementById("cont").innerHTML = rest;
     } else {
-        document.getElementById("message").value = valor.substr(0,quant);
+        document.getElementById("message").value = valor.substr(0,limit);
     }
-    // let userMessage= document.getElementById("message").value;
-    // console.log(userMessage);
-    // if(userMessage.lenght !== 0){
-    // document.getElementById("button1").disabled=false;
-    // }
-    // else{
-    //     document.getElementById("button1").disabled=true;
-    // }
 }
-function printText(){
 
-let node = document.createElement("li") 
+const test = document.getElementById("button1")
+test.addEventListener("mouseover", lockbutton);
+function lockbutton(){
+     let userMessage= document.getElementById("message").value;
+     console.log(userMessage);
+     if(userMessage.lenght !== 0){
+     document.getElementById("button1").disabled=false;
+     }
+     else{
+         document.getElementById("button1").disabled=true;
+     }
+    }
+
+document.getElementById("button1").addEventListener("onclick", printText);
+
+function printText(){
+let node = document.createElement("li"); 
 let userMessage = document.getElementById("message").value;
 let textnode= document.createTextNode(userMessage);
 node.appendChild(textnode);
 document.getElementById("tweets").appendChild(node);
 document.getElementById("textForm").reset(); 
-
 }   
