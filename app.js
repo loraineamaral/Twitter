30fa218dd5f33361c53431a534b9bedb56039938
loraineamaral <loraineeamaral@gmail.com>
2019-02-19 22:44:01 -0300

    appendChild included

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 
+function limite_textarea(valor) {
+    quant = 140;
+    total = valor.length;
+    if(total <= quant) {
+        resto = quant - total;
+        document.getElementById("cont").innerHTML = resto;
+    } else {
+        document.getElementById("message").value = valor.substr(0,quant);
+    }
+    // let userMessage= document.getElementById("message").value;
+    // console.log(userMessage);
+    // if(userMessage.lenght !== 0){
+    // document.getElementById("button1").disabled=false;
+    // }
+    // else{
+    //     document.getElementById("button1").disabled=true;
+    // }
+}
 function printText(){
-    
+
+let node = document.createElement("li") 
 let userMessage = document.getElementById("message").value;
-document.getElementById("printedMessage").textContent = userMessage;  
+let textnode= document.createTextNode(userMessage);
+node.appendChild(textnode);
+document.getElementById("tweets").appendChild(node);
 document.getElementById("textForm").reset(); 
-}                                               
+
+}   
