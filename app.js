d2e832a4d6d4a82bdd63154c3e19106c72b6cf7f
loraineamaral <loraineeamaral@gmail.com>
2019-02-20 14:02:48 -0300

    início versão 3

--- app.js
+++ app.js
@@ -2,12 +2,8 @@
 function counter(valor) {
     limit = 140;
     total = valor.length;
-    if(total <= limit) {
         rest = limit - total;
         document.getElementById("cont").innerHTML = rest;
-    } else {
-        document.getElementById("message").value = valor.substr(0,limit);
-    }
 }
 
 const test = document.getElementById("button1")
@@ -15,16 +11,18 @@ test.addEventListener("mouseover", lockbutton);
 function lockbutton(){
      let userMessage= document.getElementById("message").value;
      console.log(userMessage);
-     if(userMessage.lenght !== 0){
+     if(userMessage.length !== 0){
      document.getElementById("button1").disabled=false;
      }
      else{
          document.getElementById("button1").disabled=true;
      }
+     if(userMessage.length >= 140){
+     document.getElementById("button1").disabled=true;
+     }
     }
 
-document.getElementById("button1").addEventListener("onclick", printText);
-
+    document.getElementById("button1").addEventListener('click', printText);
 function printText(){
 let node = document.createElement("li"); 
 let userMessage = document.getElementById("message").value;
